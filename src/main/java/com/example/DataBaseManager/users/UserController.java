230c2f7cb612aea8ea4bd2ec1ee9aa8e71b7f009
merajuni7ed <merajmusavi1243m@gmail.com>
2023-12-20 19:39:44 +0330

    baseline

---
+++ src/main/java/com/example/DataBaseManager/users/UserController.java
@@ -0,0 +1,9 @@
+package com.example.DataBaseManager.users;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+
+}
---
+++ src/main/java/com/example/DataBaseManager/users/UserService.java
@@ -0,0 +1,7 @@
+package com.example.DataBaseManager.users;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}

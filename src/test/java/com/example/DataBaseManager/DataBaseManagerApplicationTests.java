19144695331a439d3b93493e372d71986db37487
merajuni7ed <merajmusavi1243m@gmail.com>
2023-12-20 19:35:31 +0330

    Initial commit

---
+++ src/test/java/com/example/DataBaseManager/DataBaseManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DataBaseManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataBaseManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
